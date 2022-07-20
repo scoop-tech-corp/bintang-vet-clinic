d85fbabb7e70c7829737458b8e3a4586972feb02
Project-web-code-program <scooptechcorp@gmail.com>
2022-07-20 22:26:36 +0700

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -25,5 +25,15 @@ class UserSeeder extends Seeder
             'created_by' => 'admin',
             'created_at' => '2022-07-20'
         ]);
+
+        DB::table('branches')->insert([
+            'branch_code' => '123',
+            'branch_name' => 'Test',
+            'isDeleted' => 0,
+            'user_id' => 1,
+            'user_update_id' => 1,
+            'created_at' => '2022-07-20',
+            'address' =>'test address'
+        ]);
     }
 }
