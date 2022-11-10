f1ec7f74b36d482bbbbd21d9a4e46ee7eb7fc5ec
Project-web-code-program <71145562+scoop-tech-corp@users.noreply.github.com>
2022-11-11 00:55:06 +0700

    Fixing Issue Upload Harga Barang Pet SHop

--- app/Imports/HargaBarangImport.php
+++ app/Imports/HargaBarangImport.php
@@ -3,36 +3,43 @@
 namespace App\Imports;
 
 use App\Models\PriceItem;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithValidation;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
 class HargaBarangImport implements ToModel, WithHeadingRow, WithValidation
 {
-  use Importable;
+    use Importable;
 
-  public function model(array $row)
-  {
+    protected $id;
 
-      return new PriceItem([
-          'list_of_items_id' => $row['kode_daftar_barang'],
-          'selling_price' => $row['harga_jual'],
-          'capital_price' => $row['harga_modal'],
-          'doctor_fee' => $row['fee_dokter'],
-          'petshop_fee' => $row['fee_petshop'],
-          'user_id' => 1,
-      ]);
-  }
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
 
-  public function rules(): array
-  {
-      return [
-          '*.kode_daftar_barang' => 'required|integer',
-          '*.harga_jual' => 'required|numeric',
-          '*.harga_modal' => 'required|numeric',
-          '*.fee_dokter' => 'required|numeric',
-          '*.fee_petshop' => 'required|numeric',
-      ];
-  }
+    public function model(array $row)
+    {
+
+        return new PriceItem([
+            'list_of_items_id' => $row['kode_daftar_barang'],
+            'selling_price' => $row['harga_jual'],
+            'capital_price' => $row['harga_modal'],
+            'doctor_fee' => $row['fee_dokter'],
+            'petshop_fee' => $row['fee_petshop'],
+            'user_id' => $this->id,
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            '*.kode_daftar_barang' => 'required|integer',
+            '*.harga_jual' => 'required|numeric',
+            '*.harga_modal' => 'required|numeric',
+            '*.fee_dokter' => 'required|numeric',
+            '*.fee_petshop' => 'required|numeric',
+        ];
+    }
 }
