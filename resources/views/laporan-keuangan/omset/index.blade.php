1dd9e27e72909c3e0ce9618cbfb18981f9232414
ahmedmutar <scooptechcorp@gmail.com>
2026-06-30 11:45:41 +0700

    Fixing Omset

--- resources/views/laporan-keuangan/omset/index.blade.php
+++ resources/views/laporan-keuangan/omset/index.blade.php
@@ -15,15 +15,24 @@
         </div>
 
         <div id="filter-mingguan" class="filter-periode-input" style="display:none;">
-          <input type="date" id="startDate" class="form-control input-sm">
-          <span class="filter-sep">s/d</span>
-          <input type="date" id="endDate" class="form-control input-sm">
+          <div class="input-group date omset-range-group">
+            <div class="input-group-addon" style="cursor:pointer;">
+              <i class="fa fa-calendar"></i>
+            </div>
+            <input type="text" class="form-control omset-range-input" id="datepicker-omset-range"
+                   placeholder="yyyy-mm-dd - yyyy-mm-dd" autocomplete="off" readonly>
+          </div>
         </div>
 
         <div id="filter-bulanan" class="filter-periode-input" style="display:none;">
           <div class="input-group input-group-sm filter-bulanan-group">
-            <input type="text" id="inputBulan" class="form-control" placeholder="mm-yyyy" readonly style="cursor:pointer;">
-            <span class="input-group-addon" style="cursor:pointer;"><i class="fa fa-calendar"></i></span>
+            <input type="text" id="inputBulanFrom" class="form-control" placeholder="mm-yyyy" readonly style="cursor:pointer;">
+            <span class="input-group-addon" id="addonBulanFrom" style="cursor:pointer;"><i class="fa fa-calendar"></i></span>
+          </div>
+          <span class="filter-sep">s/d</span>
+          <div class="input-group input-group-sm filter-bulanan-group">
+            <input type="text" id="inputBulanTo" class="form-control" placeholder="mm-yyyy" readonly style="cursor:pointer;">
+            <span class="input-group-addon" id="addonBulanTo" style="cursor:pointer;"><i class="fa fa-calendar"></i></span>
           </div>
         </div>
 
@@ -36,7 +45,9 @@
         <button type="button" id="btn-export-omset" class="btn btn-success btn-sm" title="Generate Excel" style="display:none;">
           <i class="fa fa-file-excel-o" aria-hidden="true"></i>&nbsp;&nbsp;Generate Excel
         </button>
-        <select id="filterCabang" class="filter-branch"></select>
+        <div id="cabang-filter-wrapper">
+          <select id="filterCabang" class="filter-branch"></select>
+        </div>
       </div>
     </div>
   </div>
@@ -62,12 +73,15 @@
 @endsection
 @section('script-content')
 <script src="{{ asset('plugins/highcharts/highstock.js') }}"></script>
+<script src="{{ asset('bower_components/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
 <script src="{{ asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
 <script src="{{ asset('main/js/laporan-keuangan/omset/omset.js') }}"></script>
 @endsection
 @section('css-content')
 <link rel="stylesheet" type='text/css' href="{{ asset('main/css/rekap.css') }}">
 <link rel="stylesheet" href="{{ asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
 @endsection
 @section('vue-content')
 @endsection
